4a5382b6bc3d97f2b2f85e41a0b698aa31ed0c20
rupali.meshram <meshram.rupali86@gmail.com>
2018-02-03 11:03:54 -0500

    sign in

--- assets/app.js
+++ assets/app.js
@@ -50,6 +50,44 @@ var config = {
       .html('Google ');
   });
 // ================================================================================================================================
+// ===================================sign in github============================================================================
+    
+      // sign in using google
+    $(document).on('click', '.signIn', function() {
+    
+   var provider = new firebase.auth.GithubAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+    $(this).removeClass('signIn')
+      .addClass('signOut')
+      .html('Sign Out ');
+  });
+
+    $(document).on('click', '.signOut', function () {
+    firebase.auth().signOut().then(function() {
+      $('.container ').hide();
+    }, function(error) {
+      // An error happened.
+    });
+    $(this).removeClass('signOut')
+      .addClass('signIn')
+      .html('Github ');
+  });
+// ================================================================================================================================
 var database = firebase.database();
 
 // 2. Button for adding Train Schedule
